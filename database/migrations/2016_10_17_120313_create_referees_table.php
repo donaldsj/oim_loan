7950aafb37e9aa0c8d4917498f0fe11572787064
Donald Samwel <donald.samwel@ega.go.tz>
2016-10-20 17:49:51 +0300

    fix referees table migration

--- database/migrations/2016_10_17_120313_create_referees_table.php
+++ database/migrations/2016_10_17_120313_create_referees_table.php
@@ -15,7 +15,7 @@ class CreateRefereesTable extends Migration
     {
         Schema::create('referees', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('customer_id', 255);
+            $table->integer('customer_id');
             $table->string('first_name', 50);
             $table->string('second_name', 50)->nullable();
             $table->string('last_name', 50);
